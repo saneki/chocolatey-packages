283a1c0856f3459509a22a5efea974f24a4d1a35
saneki <s@neki.me>
2015-08-05 18:25:17 -0500

    [de4dot] Added incomplete de4dot files

---
+++ de4dot/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageName = 'de4dot'
+$buildId = 'TODO'
+$url = 'https://ci.appveyor.com/api/buildjobs/{0}/artifacts/de4dot.zip' `
+       -f $buildId
+$checksum = 'TODO'
+$checksumType = 'sha1'
+$unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -UnzipLocation "$unzipLocation"
