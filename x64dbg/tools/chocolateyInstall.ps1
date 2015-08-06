cdd9ede76c4998cfc760ed809ff558922f1f56a3
saneki <s@neki.me>
2015-08-06 18:05:05 -0500

    [x64dbg] Install common programs shortcuts for x32dbg.exe/x64dbg.exe

--- x64dbg/tools/chocolateyInstall.ps1
+++ x64dbg/tools/chocolateyInstall.ps1
@@ -7,8 +7,60 @@ $checksum = '09e2864088ba2c19e1d6dd82231e3b8049611f02'
 $checksumType = 'sha1'
 $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
+# Currently copied from dnSpy chocolateyInstall.ps1 script, unsure of a way
+# to make this more portable/includable from an external file?
+function Install-CommonProgramsShortcut($file, $path='')
+{
+	if (!$file)
+	{
+		Write-Error 'Unable to install common programs shortcut, no file provided'
+		return
+	}
+
+	$commonPrograms = [Environment]::GetFolderPath("CommonPrograms")
+	if (!$commonPrograms)
+	{
+		Write-Error 'Unable to install common programs shortcut, could not get CommonPrograms path'
+		return
+	}
+
+	$fileLnk = [IO.Path]::GetFileNameWithoutExtension($file) + '.lnk'
+	$fileLnk = [IO.Path]::GetFileName($fileLnk)
+	$targetPath = Join-Path -Path "$unzipLocation" -ChildPath "$file"
+	$shortcutPath = Join-Path -Path "$commonPrograms" -ChildPath "$path" `
+	              | Join-Path -ChildPath "$fileLnk"
+
+	# Create shortcut directory if it doesn't exist
+	$shortcutDir = [IO.Path]::GetDirectoryName($shortcutPath)
+	if (!(Test-Path -Path "$shortcutDir"))
+	{
+		New-Item -ItemType "directory" -Path "$shortcutDir"
+	}
+
+	Install-ChocolateyShortcut -ShortcutFilePath "$shortcutPath" `
+	                           -TargetPath "$targetPath"
+}
+
+function Get-X64DbgExecutablePath($x)
+{
+	if (!$x)
+	{
+		Write-Error 'No parameter provided'
+		return
+	}
+
+	return Join-Path -Path 'release' -ChildPath "$x" `
+	     | Join-Path -ChildPath ('{0}dbg.exe' -f $x)
+}
+
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url" `
                              -Checksum "$checksum" `
                              -ChecksumType "$checksumType" `
                              -UnzipLocation "$unzipLocation"
+
+# Install common programs shortcuts for x32dbg.exe, x64dbg.exe
+$x32Path = Get-X64DbgExecutablePath 'x32'
+$x64Path = Get-X64DbgExecutablePath 'x64'
+Install-CommonProgramsShortcut -Path 'x64dbg' -File "$x32Path"
+Install-CommonProgramsShortcut -Path 'x64dbg' -File "$x64Path"
---
+++ x64dbg/tools/chocolateyUninstall.ps1
@@ -0,0 +1,27 @@
+﻿# Currently copied from dnSpy chocolateyUninstall.ps1 script, unsure of a way
+# to make this more portable/includable from an external file?
+function Remove-CommonProgramsDirectory($path)
+{
+	if (!$path)
+	{
+		Write-Error "Unable to remove common programs shortcut, no path provided"
+		return
+	}
+
+	$commonPrograms = [Environment]::GetFolderPath("CommonPrograms")
+	if (!$commonPrograms)
+	{
+		Write-Error "Unable to remove common programs shortcut, could not get CommonPrograms path"
+		return
+	}
+
+	$directory = Join-Path -Path "$commonPrograms" -ChildPath "$path"
+	if ((Test-Path -Path "$directory") -and `
+	([IO.Path]::GetFileName("$directory") -ceq "$path"))
+	{
+		Remove-Item -Recurse "$directory"
+	}
+}
+
+# Remove the start menu (common programs) directory
+Remove-CommonProgramsDirectory "x64dbg"
