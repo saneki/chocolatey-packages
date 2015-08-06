ac40e6e2752337d3c99ab746f77c1b61e0edea6a
saneki <s@neki.me>
2015-08-05 20:05:57 -0500

    [de4dot] Temporary url/version

--- de4dot/tools/chocolateyInstall.ps1
+++ de4dot/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'de4dot'
-$buildId = 'TODO'
+$buildId = '0k17rd8uyhsh937o'
 $url = 'https://ci.appveyor.com/api/buildjobs/{0}/artifacts/de4dot.zip' `
        -f $buildId
-$checksum = 'TODO'
+$checksum = '2bff89e1cb9e55108ec1499ce8cad1025f046238'
 $checksumType = 'sha1'
 $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
