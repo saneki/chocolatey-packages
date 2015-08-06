111ff4a0d60ca8ca7ed2966179ef91b453def344
saneki <s@neki.me>
2015-08-06 17:23:57 -0500

    [x64dbg] Added basic x64dbg files

---
+++ x64dbg/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageName = 'x64dbg'
+$mirror = 'iweb'
+$snapshotDate = '2015-07-14_02-09'
+$url = 'http://{0}.dl.sourceforge.net/project/x64dbg/snapshots/snapshot_{1}.7z' `
+       -f $mirror, $snapshotDate
+$checksum = '09e2864088ba2c19e1d6dd82231e3b8049611f02'
+$checksumType = 'sha1'
+$unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType" `
+                             -UnzipLocation "$unzipLocation"
