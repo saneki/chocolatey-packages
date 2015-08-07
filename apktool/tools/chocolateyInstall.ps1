4e341eee36b97a8e1b719c142e3716d9f5885582
saneki <s@neki.me>
2015-08-07 01:23:15 -0500

    [apktool] Added apktool files (2.0.1)

---
+++ apktool/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$packageName = 'apktool'
+$version = '2.0.1'
+$url = 'https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_{0}.jar' `
+       -f $version
+$checksum = 'dc411df2fd6cdcb5e5efbbd407f45ab80969914c'
+$checksumType = 'sha1'
+$fileFullPath = Join-Path -Path $env:ChocolateyInstall -ChildPath 'lib' `
+              | Join-Path -ChildPath "$packageName" `
+              | Join-Path -ChildPath 'tools' `
+              | Join-Path -ChildPath ('{0}.jar' -f $packageName)
+
+Get-ChocolateyWebFile -PackageName "$packageName" `
+                      -FileFullPath "$fileFullPath" `
+                      -Url "$url" `
+                      -Checksum "$checksum" `
+                      -ChecksumType "$checksumType"
