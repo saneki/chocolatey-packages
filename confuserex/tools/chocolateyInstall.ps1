af7ff71f0e99b4a135b7ae19ff933388cd07361c
saneki <s@neki.me>
2015-10-27 12:59:09 -0700

    [confuserex] Added confuserex files

---
+++ confuserex/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = 'confuserex'
+$version = '0.5.0'
+$url = 'https://github.com/yck1509/ConfuserEx/releases/download/v{0}/ConfuserEx_bin.zip' `
+       -f $version
+$unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$unzipLocation"
