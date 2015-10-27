8313ef6a5fb193c7a9e67ab42684ee15f6e85393
saneki <s@neki.me>
2015-10-27 13:24:15 -0700

    [confuser] Added confuser files

---
+++ confuser/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'confuser'
+$url = 'https://confuser.codeplex.com/downloads/get/404433'
+$unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$unzipLocation"
