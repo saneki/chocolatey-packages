c04e1a174f55e3d9741b22df521e5826bc8580d1
saneki <s@neki.me>
2016-01-07 14:10:38 -0800

    [confuserex] Updated for 0.6.0

--- confuserex/tools/chocolateyInstall.ps1
+++ confuserex/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$packageName = 'confuserex'
-$version = '0.5.0'
+$version = '0.6.0'
 $url = 'https://github.com/yck1509/ConfuserEx/releases/download/v{0}/ConfuserEx_bin.zip' `
        -f $version
 $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
