680efab1b59ce1f2c163b9655cf89b47028cb951
saneki <s@neki.me>
2016-01-09 21:53:22 -0800

    [ilrepack] Updated
    Bumped version to 2.0.10, removed checksum check, specified utf-8 in nuspec

--- ilrepack/tools/chocolateyInstall.ps1
+++ ilrepack/tools/chocolateyInstall.ps1
@@ -1,13 +1,9 @@
 ﻿$packageName = 'ilrepack'
-$version = '2.0.4'
+$version = '2.0.10'
 $url = 'https://www.nuget.org/api/v2/package/ILRepack/{0}' `
        -f $version
-$checksum = '4b3c2116b8fcd7980f40ec75cb29f99fec9a9c96'
-$checksumType = 'sha1'
 $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url" `
-                             -Checksum "$checksum" `
-                             -ChecksumType "$checksumType" `
                              -UnzipLocation "$unzipLocation"
