08d008551471c8caed786a0edd67c82bc2801679
saneki <s@neki.me>
2017-05-26 01:24:04 -0400

    [de4dot] Updated for 0.0.0.15
    Added dependency dotnet4.0 and removed hash check

--- de4dot/tools/chocolateyInstall.ps1
+++ de4dot/tools/chocolateyInstall.ps1
@@ -1,13 +1,9 @@
 ﻿$packageName = 'de4dot'
-$buildId = 'k1noivvgb93ju9y6'
+$buildId = '8ej2vtftstr9v4yw'
 $url = 'https://ci.appveyor.com/api/buildjobs/{0}/artifacts/de4dot.zip' `
        -f $buildId
-$checksum = '67d2e6223084b910755d1cea55705ea253d9d3b2'
-$checksumType = 'sha1'
 $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
                              -Url "$url" `
-                             -Checksum "$checksum" `
-                             -ChecksumType "$checksumType" `
                              -UnzipLocation "$unzipLocation"
